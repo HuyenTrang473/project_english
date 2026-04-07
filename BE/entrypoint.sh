c9195b20fbfb146db21b1ddbae56d68cb25324e9
HuyenTrang <dangmaihuyentrang@gmail.com>
2026-04-07 20:38:22 +0700

    feat: Update Dockerfile and entrypoint script to set fixed port and improve Apache configuration

--- BE/entrypoint.sh
+++ BE/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+set -e
+
+PORT="${PORT:-10000}"
+
+sed -i "s/Listen 80/Listen ${PORT}/g" /etc/apache2/ports.conf
+sed -i "s!<VirtualHost \*:80>!<VirtualHost *:${PORT}>!" /etc/apache2/sites-available/000-default.conf
+
 echo "🚀 Starting Laravel application..."
 
 # Cache config & routes
